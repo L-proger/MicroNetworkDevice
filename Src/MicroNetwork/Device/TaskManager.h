e34cc869e866cb79b6bd6c97effceac6c1ff2940
l-proger <l-proger@yandex.ru>
2020-11-28 17:09:02 +0300

    Move files from LFramework

---
+++ Src/MicroNetwork/Device/TaskManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <MicroNetwork/Common/Packet.h>
+#include <MicroNetwork/Device/ITaskContext.h>
+#include <MicroNetwork/Common/IDataReceiver.h>
+
+namespace MicroNetwork::Device {
+
+    class Task : public Common::IDataReceiver {
+	public:
+		virtual ~Task() = default;
+		virtual void run(ITaskContext* context) = 0;
+	};
+
+	class TaskManager {
+	public:
+		virtual ~TaskManager() = default;
+		virtual Task* createTask() = 0;
+		virtual void deleteTask(Task* task) = 0;
+	};
+
+}
